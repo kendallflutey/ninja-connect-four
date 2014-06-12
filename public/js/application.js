2420f7380e4580dcf3d898f5caeda47b399bd0b5
Hannah Salmon <hr.salmon@gmail.com>
2014-06-12 17:27:26 +1200

    Users can place alternating pieces

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,5 @@
-$(document).ready(function() {
+window.onload = function(){
 
-  $("table").on("click", function () {
-    var emptyCell = $(this).find("td.empty:last");
-    emptyCell.removeClass("empty");
-    emptyCell.addClass("red");
-  })
+  var view = new ConnectFourView("table");
 
-	// var model = new ConnectFourModel();
-	// var view = new ConnectFourView("#movePirateShip", "#pirateShipImage");
-
-	// var controller = new ConnectFourController(model, view);
-
-});
-
+}
