294dfa7f5c09a54b21296091c54e6c1cc85ec1de
Kendall Flutey <kendallflutey@me.com>
2014-06-12 17:39:37 +1200

    Finished initial matrix

--- models/connectFourModel.js
+++ models/connectFourModel.js
@@ -1,15 +1,46 @@
-function PirateShipModel() {
-this.location = 0;
-}
- 
-PirateShipModel.prototype.GetLocationOfShip = function() {
-this.location += 10;
-return this.location;
-};
-
 function ConnectFourModel() {
 
+	var board = [[0,0,0,0,0,0,0],
+							 [0,0,0,0,0,0,0],
+							 [0,0,0,0,0,0,0],
+							 [0,0,0,0,0,0,0],
+							 [0,0,0,0,0,0,0],
+							 [1,0,0,0,0,0,0]];
+
+	var red = 1;
+	var yellow = 2;
+
 };
+
+ConnectFourModel.prototype.UpdateCell = function(ColumnNumber, PlayerColour) {
+	
+	var colNum = {
+		"column-1": 0,
+		"column-2": 1,
+		"column-3": 2,
+		"column-4": 3,
+		"column-5": 4,
+		"column-6": 5,
+		"column-7": 6
+	};
+
+	var LowZero = board.length();
+	
+	for(var i = LowZero; i > 0; i--) {
+		if board[i, column[ColumnNumber]] === 0 {
+			LowZero = i;
+		};
+	};
+	board[LowZero,column[ColumnNumber]] = PlayerColour;
+};
+
+
+	// var LowZero = ColumnNumber.transpose.lastIndexOf("0")
+
+
+
+
+
 
 ConnectFourModel.prototype.GameState = function() {
 	// "recieve state from view via controller"
